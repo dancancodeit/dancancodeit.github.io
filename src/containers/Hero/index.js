34e1923fad8abdc3dbb48e7bf544679cabddb88b
Daniel Miller <dancancodeit@gmail.com>
2022-04-25 07:04:47 -0400

    rework + mobile resposiveness

--- src/containers/Hero/index.js
+++ src/containers/Hero/index.js
@@ -8,18 +8,18 @@ function Hero(props) {
       <a href="https://twitter.com/dancancodeit" className="social-item link">Twitter</a>
       <a href="https://www.tiktok.com/@codingproblems" className="social-item link">TikTok</a>
       <a href="https://www.instagram.com/dancancodeit/" className="social-item link">Instagram</a>
+      <a className="social-item link" href="https://medium.com/@dancancodeit">Blog</a>
+      <a className="social-item link" href="https://github.com/dancancodeit">Github</a>
+      <a className="social-item link" href="https://www.youtube.com/channel/UCsymgNWEdy7qQuVz3jGOAbA">Youtube</a>
+      <a className="social-item link" href="mailto:hello@dancancodeit.com">Contact</a>
     </div>
-		<div class="hero-container">
-      <div class="name-container">
-        <span>Daniel Miller</span>
+    <div className="ham"><h2>≡</h2></div>
+		<div className="hero-container">
+      <div className="name-container">
+        <h2 className="main-text">Hello, I'm Daniel Miller</h2>
+        <h5 className="sub-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque magna justo, sit amet pharetra urna aliquam ut.</h5> 
       </div>
-        <img class="me-pic" src={me} alt="me"/>
-      <div class="nav">
-        <a className="link" href="https://medium.com/@dancancodeit">Blog</a>
-        <a className="link" href="https://github.com/dancancodeit">Github</a>
-        <a className="link" href="https://www.youtube.com/channel/UCsymgNWEdy7qQuVz3jGOAbA">Youtube</a>
-        <a className="link" href="mailto:hello@dancancodeit.com">Contact</a>
-      </div>
+      <img className="me-pic" src={me} alt="me"/>
 		</div>
 
 	</div>
