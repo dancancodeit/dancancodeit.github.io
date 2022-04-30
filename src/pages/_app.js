c7bbf6181ea9747bdb12a01afd50420ca96da990
Daniel Miller <dancancodeit@gmail.com>
2022-04-29 23:55:41 -0400

    more files

---
+++ src/pages/_app.js
@@ -0,0 +1,19 @@
+import '../styles.css'
+import '../App.css';
+import '../index.css'
+
+import Hero from './index.js'
+import Particles from "react-tsparticles";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Particles url="/particles.json" canvasClassName="particles-canvas" className='particles' style={{zIndex: '-1'}}/>
+        <Hero/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react"
+
+
+
+function Hero(props) {
+  const [ isOpen, setOpen ] = useState(false);
+  const toggleMenu = () => setOpen(!isOpen);
+  const fadeStyle = { 'opacity': isOpen ? 1 : 0}
+
+	return <div className="container">
+    <div className="socials">
+      <a className="social-item link" href="https://github.com/dancancodeit">Github</a>
+      <a className="social-item link" href="https://medium.com/@dancancodeit">Blog</a>
+      <a href="https://twitter.com/dancancodeit" className="social-item link">Twitter</a>
+      <a className="social-item link" href="https://www.youtube.com/channel/UCsymgNWEdy7qQuVz3jGOAbA">Youtube</a>
+      <a href="https://www.instagram.com/dancancodeit/" className="social-item link">Instagram</a>
+      <a href="https://www.tiktok.com/@codingproblems" className="social-item link">TikTok</a>
+      <a className="social-item link" href="mailto:hello@dancancodeit.com">Contact</a>
+    </div>
+    <div className="ham" onClick={toggleMenu}><h2>≡</h2></div>
+    <div id="mobile-drop" style={fadeStyle}>
+      <div className="mobile-link">Github</div>
+      <div className="mobile-link">Blog</div>
+      <div className="mobile-link">Twitter</div>
+      <div className="mobile-link">Youtube</div>
+      <div className="mobile-link">Instagram</div>
+      <div className="mobile-link">TikTok</div>
+      <div className="mobile-link">Contact</div>
+    </div>
+		<div className="hero-container">
+      <div className="name-container">
+        <h2 className="main-text">Hello, I'm Daniel Miller</h2>
+        <h5 className="sub-text">Despite this awesome looking website, my passion lies in building highly scalable distributed systems (getting this website to look like this took way too long)</h5> 
+      </div>
+      <img className="me-pic" src="/rocket-me.svg" alt="me"/>
+		</div>
+
+	</div>
+}
+
+export default Hero;
