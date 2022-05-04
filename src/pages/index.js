543034a543e6d548dcc56f1128245ffa3dec22e0
Daniel Miller <dancancodeit@gmail.com>
2022-05-04 06:31:52 -0400

    fix links

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,18 +15,18 @@ function Hero(props) {
       <a href="https://twitter.com/dancancodeit" className="social-item link">Twitter</a>
       <a className="social-item link" href="https://www.youtube.com/channel/UCsymgNWEdy7qQuVz3jGOAbA">Youtube</a>
       <a href="https://www.instagram.com/dancancodeit/" className="social-item link">Instagram</a>
-      <a href="https:/p/www.tiktok.com/@codingproblems" className="social-item link">TikTok</a>
+      <a href="https://www.tiktok.com/@codingproblems" className="social-item link">TikTok</a>
       <a className="social-item link" href="mailto:hello@dancancodeit.com">Contact</a>
     </div>
     <div className="ham" onClick={toggleMenu}><h2>≡</h2></div>
     <div id="mobile-drop" style={fadeStyle}>
-      <div className="mobile-link">Github</div>
-      <div className="mobile-link">Blog</div>
-      <div className="mobile-link">Twitter</div>
-      <div className="mobile-link">Youtube</div>
-      <div className="mobile-link">Instagram</div>
-      <div className="mobile-link">TikTok</div>
-      <div className="mobile-link">Contact</div>
+      <a href="https://github.com/dancancodeit" className="mobile-link">Github</a>
+      <a href="https://medium.com/@dancancodeit" className="mobile-link">Blog</a>
+      <a href="https://twitter.com/dancancodeit" className="mobile-link">Twitter</a>
+      <a href="https://www.youtube.com/channel/UCsymgNWEdy7qQuVz3jGOAbA" className="mobile-link">Youtube</a>
+      <a href="https://www.instagram.com/dancancodeit/" className="mobile-link">Instagram</a>
+      <a href="https://swww.tiktok.com/@codingproblems" className="mobile-link">TikTok</a>
+      <a href="mailto:hello@dancancodeit.com" className="mobile-link">Contact</a>
     </div>
 		<div className="hero-container">
       <div className="name-container">
